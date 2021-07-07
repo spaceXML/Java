e066fed9fb18718a94ee1200d4d16abc157d59a7
kitri <kitri@DESKTOP-4G1D7HS>
2021-07-07 16:17:14 +0900

    123123

--- src/Java/Customer.java
+++ src/Java/Customer.java
@@ -2,17 +2,35 @@ package Java;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class Customer {
 	public static void main(String[] args) {
 		getConnection();
 	}
+	
+	public static void createTable () {
+		try {
+			Connection con = getConnection();
+			PreparedStatement createTable = con.prepareStatement(
+					"Create Table in not Exitsts"
+					+"customer(id int Not NuLL, Auto_Increment,"
+					+ "name varChar(255),"
+					+ "phone "
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+			
+		}
+	}
+	
 	public static Connection getConnection() {
 		try {
+			// 이부분에서 조심해야할것은 . 으로 찍을때 쉼표찍기 조심.
 			String driver = "com.mysql.cj.jdbc.Driver";
 			String url = "jdbc:mysql://sql6.freemysqlhosting.net:3306/sql6423478";
 			String user = "sql6423478";
 			String pass = "K85satcNpU";
+			// 이부분에서 조심해야할것은 . 으로 찍을때 쉼표찍기 조심.
 			Class.forName(driver);
 			Connection con = DriverManager.getConnection(url,user,pass);
 			System.out.println("The Connection Sccessful");
