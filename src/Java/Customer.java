929e3c45208674bf988756c3bfe9fe7e8b3570b4
kitri <kitri@DESKTOP-4G1D7HS>
2021-07-08 11:47:49 +0900

    20210708

--- src/Java/Customer.java
+++ src/Java/Customer.java
@@ -3,80 +3,101 @@ package Java;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.mysql.cj.x.protobuf.MysqlxPrepare.Prepare;
 
 public class Customer {
+
 	public static void main(String[] args) {
-		getConnection();
-		createCustomer("Dr.Kim","010-5555-5555","Male","62","Schedule memo....");
+//		getConnection();
+		//처음 DB연결부분확인부분
+		createTable();
+		createCustomer("Dr.kim", "123456789", "male","23" ,"Random Note....");
+		
+	}
+	public static void createCustomer(String name, String phone, String gender, String age, String note) {
+		try {
+			Connection con = getConnection();
+			PreparedStatement insert =  con.prepareStatement(""
+					+"INSERT INTO customer"
+					+"(name, phone, gender, age, note) "
+					+"VALUE "
+					+"('"+name+"','"+phone+"','"+gender+"','"+age+"','"+note+"')");
+			insert.executeUpdate();
+			// 데이터들어가는순서
+			// name+"','"+phone+"','"+gender+"','"+age+"','"+note+"'
+			System.out.println("the data has been saved!");
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}finally {
+			//마지막 콘솔이 입력되고 오류가 발생할경우 나오는 메세지의값
+			System.out.println("the data has been saved!");
+		}
+	}
+	public static void createTable() {
+		try {
+			Connection con = getConnection();
+			PreparedStatement create = con.prepareStatement (
+					"CREATE TABLE IF NOT EXISTS "
+					+ "customer(id int NOT NULL AUTO_INCREMENT,"
+					+ "name varChar(255),"
+					+ "phone varChar(255),"
+					+ "gender varChar(255),"
+					+ "age varChar(255),"
+					+ "note varChar(255),"
+					+ "PRIMARY KEY(id))");
+				create.execute();
+		}catch(Exception e){
+			//위험요소 콘솔
+			System.out.println(e.getMessage());
+		}finally {
+			//마지막 콘솔이 입력되고 오류가 발생할경우 나오는 메세지의값
+			System.out.println("Table seccessfull clear 255");
+		}
+		
 	}
 	
 //	public static ArrayList<String> getCustomers(){
 //		try {
 //			Connection con = getConnection();
-//			PreparedStatement statment = con. prepareStatement("")
-//			
-//		} catch(Exception e) {
+//			PreparedStatement statement = con.prepareStatement("Select name, phone, gender From customer");
+//			ResultSet results = statement.executeQuery();
+//			ArrayList<String> list = new ArrayList<String>();
+//			while(results.next()) {
+//				list.add("Name: " + result.getString("name")+ 
+//						" Phone"+ result.getString("phone") +
+//					    "Gender: "+ results.getString("gender"));
+//				)
+//				
+//			}
+//		}catch (Exception e) {
 //			System.out.println(e.getMessage());
 //			return null;
+//			// TODO: handle exception
 //		}
 //	}
 	
 	
 	
 	
-	public static void createCustomer (String name, String phone, String gender, String age,String note) {
+
+	public static Connection getConnection() 
+						
+					{
 		try {
-			Connection con = getConnection();
-			PreparedStatement insert = con.prepareStatement(""
-					+ "INSERT INTO customer"
-					+ "(name, phone, gender, age, note)"
-					+ " VALUE"
-					+ "('" +name+"', '"+phone+"','"+gender+"', '"+age+"','"+note+"')");
-			insert.executeUpdate();
-		   System.out.println("The data has been saved!");
-		}catch(Exception e) {
-			System.out.println(e.getMessage());
-		}
-	}
-	
-	
-	public static void createTable () {
-		try {
-			Connection con = getConnection();
-			PreparedStatement createTable = con.prepareStatement(
-					"Create Table in not Exitsts"
-					+"customer(id int Not NuLL Auto_Increment,"
-					+ "name varChar(255),"
-					+ "phone varChar(255),"
-					+ "gender varChar(255),"
-					+ "age varChar(255)," 
-					+ "note varChar(255),"
-					+ "PRIMARY KEY(id))");
-			createTable.execute();
-		}catch (Exception e) {
-			System.out.println(e.getMessage());
-			
-		}finally {
-			System.out.println("Table successfully created");
-		}
-	}
-	
-	public static Connection getConnection() {
-		try {
-			// 이부분에서 조심해야할것은 . 으로 찍을때 쉼표찍기 조심.
 			String driver = "com.mysql.cj.jdbc.Driver";
 			String url = "jdbc:mysql://sql6.freemysqlhosting.net:3306/sql6423478";
 			String user = "sql6423478";
 			String pass = "K85satcNpU";
-			// 이부분에서 조심해야할것은 . 으로 찍을때 쉼표찍기 조심.
 			Class.forName(driver);
 			Connection con = DriverManager.getConnection(url,user,pass);
-			System.out.println("The Connection Sccessful");
+			System.out.println("connettion Secessfull");
 			return con;
 		}catch(Exception e) {
 			System.out.println(e.getMessage());
 			return null;
 		}
 	}
+	
 }
-   
